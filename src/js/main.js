7fd4fa6836ed44df1755afbedb3b78d38fe2e31b
IOE <fivetwothreesix@gmail.com>
2021-06-21 00:17:08 +0900

    devel

--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,19 @@ import jsQR from "jsqr";
 
 
 var video = document.createElement("video");
-var canvasElement = document.getElementById("canvas");
-var canvas = canvasElement.getContext("2d");
+var videoCanvas = document.getElementById("videoCanvas");
 var loadingMessage = document.getElementById("loadingMessage");
 var outputContainer = document.getElementById("output");
 var outputMessage = document.getElementById("outputMessage");
 var outputData = document.getElementById("outputData");
 
-function drawLine(begin, end, color) {
-    canvas.beginPath();
-    canvas.moveTo(begin.x, begin.y);
-    canvas.lineTo(end.x, end.y);
-    canvas.lineWidth = 4;
-    canvas.strokeStyle = color;
-    canvas.stroke();
+function drawLine(ctx, begin, end, color) {
+    ctx.beginPath();
+    ctx.moveTo(begin.x, begin.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = color;
+    ctx.stroke();
 }
 
 // Use facingMode: environment to attemt to get the front camera on phones
@@ -32,29 +31,31 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).th
 function tick() {
     loadingMessage.innerText = "⌛ Loading video..."
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    loadingMessage.hidden = true;
-    canvasElement.hidden = false;
-    outputContainer.hidden = false;
+        var ctx = videoCanvas.getContext("2d");
 
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-    var code = jsQR(imageData.data, imageData.width, imageData.height, {
-        inversionAttempts: "dontInvert",
-    });
-    if (code) {
-        drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-        outputMessage.hidden = true;
-        outputData.parentElement.hidden = false;
-        outputData.innerText = code.data;
-    } else {
-        outputMessage.hidden = false;
-        outputData.parentElement.hidden = true;
-    }
+        loadingMessage.hidden = true;
+        videoCanvas.hidden = false;
+        outputContainer.hidden = false;
+
+        videoCanvas.height = video.videoHeight;
+        videoCanvas.width = video.videoWidth;
+        ctx.drawImage(video, 0, 0, videoCanvas.width, videoCanvas.height);
+        var imageData = ctx.getImageData(0, 0, videoCanvas.width, videoCanvas.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+        });
+        if (code) {
+            drawLine(ctx, code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+            drawLine(ctx, code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+            drawLine(ctx, code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+            drawLine(ctx, code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+            outputMessage.hidden = true;
+            outputData.parentElement.hidden = false;
+            outputData.innerText = code.data;
+        } else {
+            outputMessage.hidden = false;
+            outputData.parentElement.hidden = true;
+        }
     }
     requestAnimationFrame(tick);
 }
